7bbeb9b8c54ad14a062a24628f0fcb836c368db0
Khaled Mohaseb <163205650+Khaled112003@users.noreply.github.com>
2025-02-26 17:56:59 +0200

    -add app styles -add app colors -add packages

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // الألوان الرئيسية
+  static const Color primaryGreenDark = Color(0xFF1E6F5C);
+  static const Color primaryGreenLight = Color(0xFF29BB89);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF333333);
+  static const Color greyMedium = Color(0xFF777777);
+  static const Color greyLight = Color(0xFFF5F5F5);
+  
+  // ألوان إضافية
+  static const Color orange = Color(0xFFFFBF69);
+  static const Color pinkLight = Color(0xFFF8E9E9);
+  static const Color blueLight = Color(0xFFE9F1F8);
+  
+ 
+  
+}
---
+++ lib/core/themes/app_styles.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'app_colors.dart';
+
+class AppStyles {
+  // عناوين رئيسية
+  static TextStyle font26W900 = TextStyle(
+    fontSize: 26.0.sp,
+    fontWeight: FontWeight.w900,
+    color: AppColors.black,
+  );
+  
+  static TextStyle font22W800 = TextStyle(
+    fontSize: 22.0.sp,
+    fontWeight: FontWeight.w800,
+    color: AppColors.black,
+  );
+  
+  static TextStyle font18W700 = TextStyle(
+    fontSize: 18.0.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+  
+  // نصوص فرعية
+  static TextStyle font16W600 = TextStyle(
+    fontSize: 16.0.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.greyMedium,
+  );
+  
+  static TextStyle font16W500 = TextStyle(
+    fontSize: 16.0.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+  
+  static TextStyle font14W500 = TextStyle(
+    fontSize: 14.0.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.greyMedium,
+  );
+  
+  // نصوص الجسم
+  static TextStyle font16W400 = TextStyle(
+    fontSize: 16.0.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  );
+  
+  static TextStyle font14W400 = TextStyle(
+    fontSize: 14.0.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.greyMedium,
+  );
+  
+  // أنماط أزرار
+  static TextStyle font16W700White = TextStyle(
+    fontSize: 16.0.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.white,
+  );
+  
+  static TextStyle font16W700Green = TextStyle(
+    fontSize: 16.0.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primaryGreenDark,
+  );
+  
+  // أسعار
+  static TextStyle font18W800Green = TextStyle(
+    fontSize: 18.0.sp,
+    fontWeight: FontWeight.w800,
+    color: AppColors.primaryGreenDark,
+  );
+  
+  static TextStyle font14W400LineThrough = TextStyle(
+    fontSize: 14.0.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.greyMedium,
+    decoration: TextDecoration.lineThrough,
+  );
+  
+  // فئات
+  static TextStyle font14W600 = TextStyle(
+    fontSize: 14.0.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.black,
+  );
+  
+  // بحث
+  static TextStyle font14W400Grey = TextStyle(
+    fontSize: 14.0.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.greyMedium,
+  );
+  
+  // شريط تنقل
+  static TextStyle font12W500 = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+  
+  // تقييمات
+  static TextStyle font12W600 = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.black,
+  );
+}
