14fd31d9f3b1feb78ca59cfb4a871ff53698a5c2
Khaled Mohaseb <163205650+Khaled112003@users.noreply.github.com>
2025-02-27 17:40:10 +0200

    initilazation

--- lib/core/themes/app_styles.dart
+++ lib/core/themes/app_styles.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'app_colors.dart';
 
+
 class AppStyles {
   // عناوين رئيسية
   static TextStyle font26W900 = TextStyle(
